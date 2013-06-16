721e32b7ab11cab6f13ebcb3b0f4d33a6b312602
Dave Townsend <dtownsend@k12.com,townsend@patriot.net>
2013-06-16 11:53:54 -0400

    SIGN macro

---
+++ Util/DPTMath.h
@@ -0,0 +1,19 @@
+//
+//  DPTMath.h
+//  Bull Run
+//
+//  Created by Dave Townsend on 6/10/13.
+//  Copyright (c) 2013 Dave Townsend. All rights reserved.
+//
+
+#ifndef Bull_Run_DPTMath_h
+#define Bull_Run_DPTMath_h
+
+/**
+ * Returns sign of parameter (-1 if negative, 1 if zero or positive).
+ *
+ * @param x number to get sign of
+ */
+#define SIGN(x) ((x) < 0 ? -1 : 1)
+
+#endif
