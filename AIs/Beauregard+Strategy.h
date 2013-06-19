12b8b3b181b9aa92fcefb9e1d8e15d695528e675
Dave Townsend <dtownsend@k12.com,townsend@patriot.net>
2013-06-19 18:22:29 -0400

    added comments

--- AIs/Beauregard+Strategy.h
+++ AIs/Beauregard+Strategy.h
@@ -13,14 +13,43 @@
 @class BAUnit;
 
 
+/** 
+ * Strategy module for CSA AI. This category contains internal methods only.
+ */
 @interface Beauregard (Strategy)
 
+/**
+ * Figures out which units should be assigned to which theaters.
+ *
+ * @param game the game to analyze
+ */
 - (void)strategize:(BAGame*)game;
 
+/**
+ * Figures out which theater a given unit is in.
+ *
+ * @param unit the unit to check
+ *
+ * @return the unit's theater
+ */
 - (BRAICSATheater)computeTheaterOf:(BAUnit*)unit;
 
+/**
+ * Orders units which are located in one theater but strategically
+ * allocated to the other theater.
+ */
 - (void)conductStrategicMovement;
 
+/**
+ * Assigns the orders needed to move a unit to a given destination.
+ * A maximum of two hexes are ordered, since no unit can move more than
+ * that number of hexes in any given turn.
+ *
+ * @param unit the unit to order
+ * @param destination the destination hex
+ *
+ * TODO: probably belongs in Tactics, actually
+ */
 - (void)routeUnit:(BAUnit*)unit toDestination:(HMHex)destination;
 
 @end
--- AIs/Beauregard+Tactics.h
+++ AIs/Beauregard+Tactics.h
@@ -8,11 +8,27 @@
 
 #import "Beauregard.h"
 
+
 @class BAAIInfluenceMap;
 
+
+/**
+ * Tactical module for CSA AI. This category contains internal methods only.
+ */
 @interface Beauregard (Tactics)
 
+/**
+ * Assigns an attacker, if possible.
+ *
+ * @return `YES` if an attacker was assigned, `NO` if no attack was possible
+ */
 - (BOOL)assignAttacker;
+
+/**
+ * Assigns a defender, if possible.
+ *
+ * @return `YES` if a defender was assigned, `NO` if not possible
+ */
 - (BOOL)assignDefender:(BAAIInfluenceMap*)imap;
 
 @end
