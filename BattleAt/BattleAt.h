4affa137bfe76d0da06bfd39ec20c3c1cf1742a5
Dave Townsend <dtownsend@k12.com,townsend@patriot.net>
2013-07-03 09:36:01 -0400

    commented out all DEBUGs

--- BattleAt/BattleAt.h
+++ BattleAt/BattleAt.h
@@ -22,13 +22,13 @@ typedef enum {
 #define IsOffensiveMode(mode) \
 ((mode) == CHARGE || (mode) == ATTACK || (mode) == SKIRMISH)
 
-#define DEBUG_AI(fmt, ...)             NSLog(fmt, ## __VA_ARGS__)
+#define DEBUG_AI(fmt, ...)             //NSLog(fmt, ## __VA_ARGS__)
 #define DEBUG_ANIMATION(fmt, ...)      //NSLog(fmt, ## __VA_ARGS__)
 #define DEBUG_COMBAT(fmt, ...)         //NSLog(fmt, ## __VA_ARGS__)
 #define DEBUG_MAP(fmt, ...)            //NSLog(fmt, ## __VA_ARGS__)
 #define DEBUG_MOVEMENT(fmt, ...)       //NSLog(fmt, ## __VA_ARGS__)
 #define DEBUG_MOVEORDERS(fmt, ...)     //NSLog(fmt, ## __VA_ARGS__)
-#define DEBUG_REINFORCEMENTS(fmt, ...) NSLog(fmt, ## __VA_ARGS__)
+#define DEBUG_REINFORCEMENTS(fmt, ...) //NSLog(fmt, ## __VA_ARGS__)
 #define DEBUG_SIGHTING(fmt, ...)       //NSLog(fmt, ## __VA_ARGS__)
 
 #import "BATAnimationGunfire.h"
