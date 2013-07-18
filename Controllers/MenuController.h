b48193c864c1a97f4593c3964bfeae307d766bc1
Dave Townsend <dtownsend@k12.com,townsend@patriot.net>
2013-07-18 19:30:36 -0400

    added documentation

--- Controllers/MenuController.h
+++ Controllers/MenuController.h
@@ -7,12 +7,33 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ * Abstracts away some of the pecadilloes of the UINavigationController.
+ * It doesn't do a lot, but it make some of the setup code a little nicer.
+ */
 @interface MenuController : NSObject
 
+/** 
+ * A `UINavigationController` instance available to clients. This is
+ * initially empty until one or more `pushController` calls are made.
+ */
 @property (nonatomic, readonly, strong) UINavigationController* navController;
 
+/**
+ * Designated initializer.
+ *
+ * @param window the window whose contents the `MenuController` is managing.
+ *
+ * @return the initialized `MenuController`
+ */
 - (id)initForWindow:(UIWindow*)window;
 
+/**
+ * Activates a view controller by pushing it to the `UINavigationController`
+ * stack.
+ *
+ * @param controller the view controller to activate
+ */
 - (void)pushController:(UIViewController*)controller;
 
 @end
